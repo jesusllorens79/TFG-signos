f254c13a001357c4c6534fdf4651b110d109c10b
jesusllorens79 <72131557+jesusllorens79@users.noreply.github.com>
2023-06-23 14:41:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,51 +3,81 @@ from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
 import time
+from cvzone.ClassificationModule import Classifier
 
 print(cv2.__version__)
 print(np.__version__)
 
-cam = cv2.VideoCapture(0) #ID de la cámara
+cap = cv2.VideoCapture(0) # ID de la cámara
 detector = HandDetector(maxHands=1)
-
+classifier = Classifier(r"C:\Users\jesus\Desktop\tfg_signos\Model\tres_keras_model.h5", 
+                        r"C:\Users\jesus\Desktop\tfg_signos\Model\tres_keras_model.txt")
 offset = 20
 imgSize = 300
-
-folder = "./Data/Z"
 counter = 0
+phrases = []
+current_phrase = ""
+labels = ["A","B","C", "D", "E" ,"F" ,"G" ,"H" ,"I", "J" ,"K" ,"L", "M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V", "W" ,"X" ,"Y" ,"Z"]
+
 
 while True:
-    success, img = cam.read()
+    success, img = cap.read()
+    if img is None:
+        break
+    imgOutput = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
-        
-        imgWhite = np.ones((imgSize,imgSize, 3), np.uint8)*255 # de 0 a 255, tipico de imagenes
-        imgCrop = img[y-offset:y + h + offset, x-offset:x + w +offset]
-        
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
+
         imgCropShape = imgCrop.shape
-        
-        aspectRatio = h/w
+
+        aspectRatio = h / w
+
         if aspectRatio > 1:
             k = imgSize / h
-            wCalculated = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCalculated, imgSize))
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize-wCalculated)/2)
-            imgWhite[:, wGap:wCalculated+wGap] = imgResize
-            
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
         else:
             k = imgSize / w
-            hCalculated = math.ceil(k*h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCalculated))
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
             imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize-hCalculated)/2)
-            imgWhite[hGap:hCalculated+hGap,:] = imgResize
-            
-        cv2.imshow("Image Crop", imgCrop)
-        cv2.imshow("Image White", imgWhite)
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
 
+        cv2.rectangle(imgOutput, (x - offset, y - offset - 50),
+                      (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y - 26), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x - offset, y - offset),
+                      (x + w + offset, y + h + offset), (255, 0, 255), 4)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1) #Milisecond wait
+        if cv2.waitKey(1) & 0xFF == ord('v'):  # Espera a que se presione la tecla "v"
+            current_phrase += labels[index]
+
+        if cv2.waitKey(1) & 0xFF == ord(' '):  # Espera a que se presione la tecla "espacio"
+            current_phrase += " "  # Agrega un espacio a la frase actual
+
+        if cv2.waitKey(1) & 0xFF == ord('s'):  # Espera a que se presione la tecla "s" para guardar la frase actual
+            phrases.append(current_phrase)
+            current_phrase = ""
+
+    # Dibujar frases en la imagen
+    y_offset = 100
+    for phrase in phrases:
+        cv2.putText(imgOutput, phrase, (50, y_offset), cv2.FONT_HERSHEY_COMPLEX, 1.5, (0, 0, 255), 2)
+        y_offset += 50
+
+    cv2.putText(imgOutput, current_phrase, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 0, 255), 3)
+    cv2.imshow("Image", imgOutput)
+    cv2.waitKey(1)
